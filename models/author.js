a0c458304036103720fc2bf0137cf663cade82ac
zhaoqiwang1 <wzqbewater@gmail.com>
2025-05-01 11:41:43 +0800

    add beijing time for answering questions.

--- models/author.js
+++ models/author.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose')
 
+
 const authorSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date(Date.now() + 8 * 60 * 60 * 1000)
+    // default: Date.now
   }
 })
 

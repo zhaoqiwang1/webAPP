39ce2919b1a162d6192d178f51cd1d3996a96a79
zhaoqiwang1 <wzqbewater@gmail.com>
2025-05-04 14:56:41 +0800

    add next button to grid items in phase1.

---
+++ public/js/phase1.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const item1Button = document.getElementById('item1Button');
+  item1Button.addEventListener('click', function () {
+      console.log('Next button was clicked!');
+      //const gridItem2 = document.querySelector('.grid-item2');
+      //gridItem2.classList.add('hidden');
+  });
+});

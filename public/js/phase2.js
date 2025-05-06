ff22685649af7f409134786b7b37020c69673aac
zhaoqiwang1 <wzqbewater@gmail.com>
2025-05-06 12:27:10 +0800

    use window object to pass phase1 answers to phase2.

--- public/js/phase2.js
+++ public/js/phase2.js
@@ -2,12 +2,12 @@ import { topicsTitle, topicsContent} from "./topicContents.js";
 
 // 查看且保证phase1得到的玩家id，玩家回答的Phase1问题的答案能够 pass 到 phase2:
 // Pass player data from EJS to JavaScript directly
-console.log(playerData.name);
-console.log(playerData.playerId);
-console.log(playerData.P1T1Q1);
-console.log(playerData.P1T2Q1);
-console.log(playerData.P1T3Q1);
-console.log(playerData.P1T4Q1);
+console.log(window.playerData.name);
+console.log(window.playerData.playerId);
+console.log(window.playerData.P1T1Q1);
+console.log(window.playerData.P1T2Q1);
+console.log(window.playerData.P1T3Q1);
+console.log(window.playerData.P1T4Q1);
 
 // Retrieve from localStorage
 const randomTopicOrder = JSON.parse(localStorage.getItem('randomTopicOrder'));

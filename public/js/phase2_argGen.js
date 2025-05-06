dd1ae3a7e76f047b7a9fb0da1f776e4247386d9e
zhaoqiwang1 <wzqbewater@gmail.com>
2025-05-06 13:06:11 +0800

    create phase2_argGen.js, make sure player data are passed in there.

---
+++ public/js/phase2_argGen.js
@@ -0,0 +1,14 @@
+// 查看且保证phase1得到的玩家id，玩家回答的Phase1问题的答案能够 pass 到 phase2:
+// Pass player data from EJS to JavaScript directly
+console.log(window.playerData.name);
+console.log(window.playerData.playerId);
+console.log(window.playerData.P1T1Q1);
+console.log(window.playerData.P1T2Q1);
+console.log(window.playerData.P1T3Q1);
+console.log(window.playerData.P1T4Q1);
+
+
+
+
+
+

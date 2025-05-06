ea4bea1b532bb81e137851de1c1dd15e213a7b8b
zhaoqiwang1 <wzqbewater@gmail.com>
2025-05-06 15:50:40 +0800

    pass player data to phase3_repQ.js

---
+++ public/js/phase3_repQ.js
@@ -0,0 +1,11 @@
+import { topicsTitle, topicsContent, topicArguments } from "./topicContents.js";
+
+// 查看且保证phase1得到的玩家id，玩家回答的Phase1问题的答案能够 pass 到 phase3_repQ:
+// Pass player data from EJS to JavaScript directly
+console.log(window.playerData.name);
+console.log(window.playerData.playerId);
+console.log(window.playerData.P1T1Q1);
+console.log(window.playerData.P1T2Q1);
+console.log(window.playerData.P1T3Q1);
+console.log(window.playerData.P1T4Q1);
+
